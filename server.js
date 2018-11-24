4496422fbf2eb70e8fb6404c4cdff49b76c35b46
Angga Ari Wijaya <anggadarkprince@gmail.com>
2018-11-24 09:51:43 +0700

    fix spaces

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ var os = require('os');
 var http = require('http');
 
 var server = http.createServer(function (request, response) {
-  if (request.url === '/favicon.ico') {
-    response.writeHead(200, {'Content-Type': 'image/x-icon'} );
-    response.end();
-    console.log('favicon requested');
-    return;
-  }
+    if (request.url === '/favicon.ico') {
+        response.writeHead(200, {'Content-Type': 'image/x-icon'});
+        response.end();
+        console.log('favicon requested');
+        return;
+    }
 
-  response.writeHead(200, {"Content-Type": "application/json"});
-  console.log('I have been hit')
-  response.end(JSON.stringify({
-      message: request.connection.remoteAddress,
-      net:os.networkInterfaces()
-  }));
+    response.writeHead(200, {"Content-Type": "application/json"});
+    console.log('I have been hit');
+    response.end(JSON.stringify({
+        message: request.connection.remoteAddress,
+        net: os.networkInterfaces()
+    }));
 });
 
 server.listen(3000);
